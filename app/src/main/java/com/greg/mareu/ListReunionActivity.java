60d5a14ad08eb1f51083b614503f91202d3994c5
GregBiglione <gregory.biglione81@gmail.com>
2020-05-02 17:49:52 +0200

    Initial commit

---
+++ app/src/main/java/com/greg/mareu/ListReunionActivity.java
@@ -0,0 +1,14 @@
+package com.greg.mareu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListReunionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
