1de47f2cc5c9d7e23fffa5f75b9a2415f5237317
GregBiglione <gregory.biglione81@gmail.com>
2020-05-23 07:09:02 +0200

    Branchement du date picker sur les Edit Text de la Date Dialog Box

--- app/src/main/java/com/greg/mareu/dialog_box/DateDialog.java
+++ app/src/main/java/com/greg/mareu/dialog_box/DateDialog.java
@@ -6,18 +6,45 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.greg.mareu.R;
+import com.greg.mareu.picker.Pick;
+
+import butterknife.BindView;
 
 public class DateDialog extends AppCompatDialogFragment {
+
+    @BindView(R.id.dialogStartDateEdit) EditText mStartEdit;
+    @BindView(R.id.dialogEndDateEdit) EditText mEndEdit;
+    private Pick mPick;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_date, null);
+
+        mStartEdit = view.findViewById(R.id.dialogStartDateEdit);
+        mEndEdit = view.findViewById(R.id.dialogEndDateEdit);
+
+        mStartEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pick.pickDate(mStartEdit, getActivity());
+            }
+        });
+
+        mEndEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pick.pickDate(mEndEdit, getActivity());
+
+            }
+        });
+
 
         builder.setTitle("Choix des dates")
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
