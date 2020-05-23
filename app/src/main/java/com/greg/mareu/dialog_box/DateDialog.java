11f09ca30b3e574735ed43f93dc8763e9704e496
GregBiglione <gregory.biglione81@gmail.com>
2020-05-23 04:48:32 +0200

    Ajout d'un layout à la Date Dialog Box

--- app/src/main/java/com/greg/mareu/dialog_box/DateDialog.java
+++ app/src/main/java/com/greg/mareu/dialog_box/DateDialog.java
@@ -4,14 +4,21 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.greg.mareu.R;
 
 public class DateDialog extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_date, null);
+
         builder.setTitle("Choix des dates")
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
@@ -24,7 +31,8 @@ public class DateDialog extends AppCompatDialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
 
                     }
-                });
+                })
+                .setView(view);
         return builder.create();
     }
 }
